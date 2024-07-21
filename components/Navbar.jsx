0cc32b266d9ef0cec75b2be1b7108c41735e8a5f
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-07-21 15:52:46 +0100

    Add option in case session isnt working

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,39 +7,39 @@ import { useSession, signOut } from "next-auth/react";
 import Image from "next/image";
 
 export default function Navbar() {
-  const [loggedIn, setLoggedIn] = useState(false);
   const { data: session } = useSession();
+  const cock = useSession()
+  console.log(session);
+  console.log("cock", cock)
 
-  useEffect(() => {
-    if (session) {
-      setLoggedIn(true);
-    } else {
-      setLoggedIn(false);
-    }
-  }, [session]);
 
   return (
     <div className="navbar bg-base-100 shadow-lg hover:shadow-none transition-shadow absolute">
       <div className="flex-1">
         <Link href="/" className="btn btn-ghost text-xl">
           <h2>
-            <span className="bg-gradient-to-r from-red-500 to-red-700 bg-clip-text text-transparent">LU</span>CribCheck
+            <span className="bg-gradient-to-r from-red-500 to-red-700 bg-clip-text text-transparent">
+              LU
+            </span>
+            CribCheck
           </h2>
         </Link>
       </div>
       <div className="flex-none gap-2">
         <SearchNavBar />
       </div>
-      {loggedIn ? (
+      {session && session.user ? (
         <div className="dropdown dropdown-end">
           <div
             tabIndex={0}
             role="button"
             className="btn btn-ghost btn-circle avatar"
           >
-            <div class="avatar online placeholder">
-              <div class="bg-neutral text-neutral-content w-10 rounded-full">
-                <span class="text-lg">{session.user.name[0]}</span>
+            <div className="avatar online placeholder">
+              <div className="bg-neutral text-neutral-content w-10 rounded-full">
+                <span className="text-lg">
+                  {session.user.name ? session.user.name[0] : "?"}
+                </span>
               </div>
             </div>
           </div>
@@ -50,7 +50,7 @@ export default function Navbar() {
             <p className="text-xl font-semibold p-2 w-full">
               Hello,{" "}
               <span className="bg-gradient-to-r from-red-500 to-red-700 bg-clip-text text-transparent">
-                {session.user.name}
+                {session?.user?.name || "?"}
               </span>
               !{" "}
             </p>
