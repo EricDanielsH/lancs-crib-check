f98f1f92baf18bcd39177de2956832008e107990
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-07-21 20:06:42 +0100

    Remove '/5' from rating

--- components/HouseStats.jsx
+++ components/HouseStats.jsx
@@ -38,10 +38,9 @@ export default function HouseStats({ house }) {
 
       <div className="stat place-items-center bg-white rounded-lg p-4 shadow-md  col-span-2">
         <div className="stat-title red-grad">Rating</div>
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-1">
           <div className="red-grad stat-value flex items-center">
             {house.rating} 
-            <span className="ml-1 text-sm">/5 </span>
           </div>
           <StarRating size={30} />
         </div>
