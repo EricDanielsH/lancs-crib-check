8aed5d6112e7a6e1d7072ccf1a31e52562f1a73f
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-07-21 19:35:44 +0100

    Add more rounded borders

--- components/OpinionCard.jsx
+++ components/OpinionCard.jsx
@@ -1,7 +1,7 @@
 import StarRating from "@/components/StarRating";
 export default function OpinionCard({ opinion }) {
   return (
-    <div key={opinion.id} className="flex flex-col gap-4 bg-base-200 mb-4 p-4 rounded">
+    <div key={opinion.id} className="flex flex-col gap-4 bg-base-200 mb-4 p-4 rounded-lg">
       <div className="flex items-center justify-between">
         <h4 className="text-2xl font-semibold">{opinion.authorName}</h4>
         <div className="flex items-center">
