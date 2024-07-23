754d6b3588132dd2f01e39d52e2ee794d89eb0ce
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-07-23 16:09:49 +0100

    Change to deployment bucket

--- lib/actions.ts
+++ lib/actions.ts
@@ -14,10 +14,10 @@ const generateFileName = (bytes = 32) =>
   crypto.randomBytes(bytes).toString("hex");
 
 const s3 = new S3Client({
-  region: process.env.AWS_BUCKET_REGION!,
+  region: process.env.MY_AWS_BUCKET_REGION!,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY!,
   },
 });
 
@@ -48,7 +48,7 @@ export async function getSignedURL(
   const userId = session?.user?.id ?? "";
 
   const putObjectCommand = new PutObjectCommand({
-    Bucket: process.env.AWS_BUCKET_NAME_LOCAL!,
+    Bucket: process.env.MY_AWS_BUCKET_NAME!,
     Key: generateFileName(),
     ContentType: type,
     ContentLength: size,
