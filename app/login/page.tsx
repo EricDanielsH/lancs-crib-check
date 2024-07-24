34162ba000a2729d1b3699266edc90ee169a8654
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-07-24 19:28:47 +0100

    Redirect users to main page if they have a session already

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,29 @@
+"use client";
 import LogInEmail from "@/components/auth/LogInEmail";
 import Title from "@/components/Title";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Login({
   searchParams,
 }: {
   searchParams: { verified: string };
 }) {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/");
+    }
+  }, [status, router]);
+
   const isVerified = searchParams.verified === "true";
   return (
     <main className="h-full flex-col flex justify-center items-center">
       <Title />
-      <LogInEmail isVerified={isVerified}/>
+      <LogInEmail isVerified={isVerified} />
     </main>
   );
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,20 @@
+"use client";
 import RegisterEmail from "@/components/auth/RegisterEmail";
 import Title from "@/components/Title";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Register() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/");
+    }
+  }, [session, status, router]);
+
   return (
     <main className="h-full flex flex-col justify-center items-center">
       <Title />
